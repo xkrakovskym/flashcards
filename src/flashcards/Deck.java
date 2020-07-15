2434503a7d6ba2c017bb466f4adb61eb6d364f44
michal.krakovsky <michal.krakovsky@exponea.com>
2020-07-15 16:05:56 +0200

    Stage 5 done

--- src/flashcards/Deck.java
+++ src/flashcards/Deck.java
@@ -4,21 +4,36 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.io.Serializable;
 
-public class Deck {
+public class Deck implements Serializable {
 
     private Map<String, String> termMap;
     private Map<String, String> definitionMap;
-    private int noOfCards;
+    private transient Iterator<Map.Entry<String, String>> deckIterator; // transient to exclude from Serialization
+    private int timesToAsk;
+    private int deckSize;
+    private flashcards.StateMachine state;
+    private String currentTerm;
+    private static final long serialVersionUID = 1452525600079957398L;
+    private static final boolean savedAsObject = true;
 
-    public Deck(int noOfCards) {
-        this.noOfCards = noOfCards;
+    public Deck() {
+        this.termMap = new HashMap<>();
+        this.definitionMap = new HashMap<>();
+        this.timesToAsk = 0;
         termMap = new LinkedHashMap<>();
         definitionMap = new LinkedHashMap<>();
+        state = flashcards.StateMachine.MAIN_MENU;
     }
 
-    public int deckSize() {
-        return noOfCards;
+    public void add(Map<String, String> termMapToAdd){
+        termMap.putAll(termMapToAdd);
+        definitionMap.clear();
+        for (var entry : termMap.entrySet()){
+            definitionMap.put(entry.getValue(), entry.getKey());
+        }
+        deckSize = termMap.entrySet().size();
     }
 
     public boolean termExists(String term){
@@ -32,6 +47,20 @@ public class Deck {
     public void addCard(String term, String definition){
         termMap.put(term, definition);
         definitionMap.put(definition, term);
+        ++deckSize;
+    }
+
+    public boolean removeCard(String term){
+        String deleteTerm = null;
+        for (var entry : termMap.entrySet()) {
+            if (entry.getKey().equalsIgnoreCase(term)) {
+                deleteTerm = termMap.remove(entry.getKey());
+                definitionMap.remove(deleteTerm);
+                --deckSize;
+                break;
+            }
+        }
+        return deleteTerm != null;
     }
 
     public Map<String, String> getTermMap() {
@@ -41,4 +70,43 @@ public class Deck {
         return definitionMap;
     }
 
+    public void setState(flashcards.StateMachine state) {
+        this.state = state;
+    }
+
+    public flashcards.StateMachine getState() {
+        return state;
+    }
+
+    public void setCurrentTerm(String currentTerm) {
+        this.currentTerm = currentTerm;
+    }
+
+    public String getCurrentTerm() {
+        return currentTerm;
+    }
+
+    public void setTimesToAsk(int timesToAsk) {
+        this.timesToAsk = timesToAsk;
+    }
+
+    public int getTimesToAsk() {
+        return timesToAsk;
+    }
+
+    public void initDealing() {
+        deckIterator = termMap.entrySet().iterator();
+    }
+
+    public Iterator<Map.Entry<String, String>> getDeckIterator() {
+        return deckIterator;
+    }
+
+    public int getDeckSize() {
+        return deckSize;
+    }
+
+    public static boolean isSavedAsObject() {
+        return savedAsObject;
+    }
 }
--- src/flashcards/DisplayUtils.java
+++ src/flashcards/DisplayUtils.java
@@ -4,21 +4,20 @@ import java.text.MessageFormat;
 
 public class DisplayUtils {
 
-    public static void noOfCardsText() {
-        System.out.print("Input the number of cards:\n" +
-                "> ");
+    public static void mainMenuText() {
+        System.out.println("Input the action (add, remove, import, export, ask, exit):");
     }
 
-    public static void displayCardTermInputText(int cardNo) {
-        System.out.print(MessageFormat.format("The card #{0} \n> ", cardNo));
+    public static void cardInputText() {
+        System.out.println("The card:");
     }
 
-    public static void displayCardDefinitionInputText(int cardNo) {
-        System.out.print(MessageFormat.format("The definition of the card #{0}: \n> ", cardNo));
+    public static void cardDefinitionInputText() {
+        System.out.println("The definition of the card:");
     }
 
-    public static void displayCardTermText(String definition) {
-        System.out.print(MessageFormat.format("Print the definition of \"{0}\": \n> ", definition));
+    public static void cardAskText(String definition) {
+        System.out.print(String.format("Print the definition of \"%s\": \n> ", definition));
     }
 
     public static void correctAnswerText() {
@@ -31,17 +30,62 @@ public class DisplayUtils {
     }
 
     public static void termAlreadyExistsText(String term) {
-        System.out.print(MessageFormat.format("The card \"{0}\" already exists. Try again: \n> ",
+        System.out.println(String.format("The card \"%s\" already exists.\n",
                 term));
     }
 
     public static void definitionAlreadyExistsText(String definition) {
-        System.out.print(MessageFormat.format("The definition \"{0}\" already exists. Try again: \n> ",
+        System.out.println(String.format("The definition \"%s\" already exists.\n",
                 definition));
     }
 
     public static void incorrectAnswerDefinitionExistsText(String correctDefinition, String guessedTerm) {
-        System.out.print(MessageFormat.format("Wrong answer. The correct one is \"{0}\", you''ve just written the" +
-                " definition of \"{1}\".\n", correctDefinition, guessedTerm));
+        System.out.println(MessageFormat.format("Wrong answer. The correct one is \"{0}\", you''ve just written the" +
+                " definition of \"{1}\".", correctDefinition, guessedTerm));
+    }
+
+    public static void incorrectInputText() {
+        System.out.println("Wrong input, please use one of these commands: add, remove, import, ask, exit");
+    }
+
+    public static void exitText() {
+        System.out.println("Bye bye!");
+    }
+
+    public static void cardWasRemovedText(){
+        System.out.println("The card has been removed.\n");
+    }
+
+    public static void cardWasNotRemovedText(String term){
+        System.out.println(String.format("Can\'t remove \"%s\": there is no such card.\n",
+                term));
+    }
+
+    public static void askHowManyTimeText(){
+        System.out.println("How many times to ask?");
+    }
+
+    public static void wrongFormatInputText(){
+        System.out.println("Wrong input!");
+    }
+
+    public static void fileNameText(){
+        System.out.println("File name:");
+    }
+
+    public static void fileErrorText(){
+        System.out.println("File not found.");
+    }
+
+    public static void cardAddedText(String term, String definition) {
+        System.out.println(String.format("The pair (\"%s\":\"%s\") has been added.\n", term, definition));
+    }
+
+    public static void savedCardsText(int numOfCards){
+        System.out.println(String.format("%d cards have been saved", numOfCards));
+    }
+
+    public static void loadedCardsText(int numOfCards){
+        System.out.println(String.format("%d cards have been loaded", numOfCards));
     }
 }
